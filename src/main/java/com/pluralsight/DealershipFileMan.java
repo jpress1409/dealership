f094418a4867c8bfb1a917113f11604312e17e63
jpress1409 <jpress1409@gmail.com>
2024-10-25 11:58:13 -0400

    Started saveDealership()

--- src/main/java/com/pluralsight/DealershipFileMan.java
+++ src/main/java/com/pluralsight/DealershipFileMan.java
@@ -1,9 +1,36 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileMan {
+    private String FILE_NAME;
     public DealershipFileMan() {
+        this.FILE_NAME = "dealership.csv";
     }
-    public void saveDealership(Dealership dealership){
+    public void saveDealership(Dealership dealership, String FILE_NAME){
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            reader.readLine();
+            while((line = reader.readLine()) != null){
+                String[] parts = line.split("\\|");
+                if(parts.length == 8){
+                    int vin = Integer.parseInt(parts[0]);
+                    int year = Integer.parseInt(parts[1]);
+                    String make = parts[2];
+                    String model = parts[3];
+                    String vehicleType = parts[4];
+                    String color = parts[5];
+                    int odometer = Integer.parseInt(parts[6]);
+                    double price = Double.parseDouble(parts[7]);
+                    double price = Double.parseDouble(parts[7]);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        };
 
     }
     public Dealership getDealership(){
